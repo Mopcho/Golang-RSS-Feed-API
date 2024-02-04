26d69f73a752ec9dfc8f3f7f7d8013d6bbd09df7
Mopcho <47301161+Mopcho@users.noreply.github.com>
2024-02-05 01:06:34 +0200

    create user in database

---
+++ handler_user.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Mopcho/Golang-api/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCnfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `name`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing json %v", err))
+		return
+	}
+
+	user, err := apiCnfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't create an user: %v", err))
+		return
+	}
+
+	respondWithJson(w, 200, databaseUserToUser(user))
+}
--- main.go
+++ main.go
@@ -1,23 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/Mopcho/Golang-api/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
-
 	if portString == "" {
 		log.Fatal("Port not bound in environment")
+	}
+
+	db_url := os.Getenv("DB_URL")
+	if db_url == "" {
+		log.Fatal("DB_URL not in environment variable")
+	}
+
+	conn, err := sql.Open("postgres", db_url)
+	if err != nil {
+		log.Fatal("Can't connect to database: ", err)
+	}
+
+	apiCnfg := apiConfig{
+		DB: database.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -35,6 +56,7 @@ func main() {
 
 	v1Router.Get("/healthz", handle_readiness)
 	v1Router.Get("/error", handleErr)
+	v1Router.Post("/users", apiCnfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -44,7 +66,7 @@ func main() {
 	}
 
 	log.Printf("Server starting on port %v", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
